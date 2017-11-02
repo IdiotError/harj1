d8358eba898138176e95719c141e0a8fbe3c75d3
Unknown <tuoppi922@hotmail.com>
2017-11-02 09:51:06 +0200

    added menu

---
+++ menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP esimerkkiprojekti</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="myStyle.css">
+  </head>
+  <body>
+      <div id="menu">
+        <ul>
+          <li><a href="index.php">Eka</a></li>
+          <li><a href="eka.php">Array esimerkki</a></li>
+          <li><a href="get_esimerkki.php">Get esimerkki</a></li>
+          <li><a href="post_esimerkki.php">Post esimerkki</a></li>
+          <li><a href="luokka_esimerkki.php">Luokka esimerkki</a></li>
+          <li><a href="tietokanta.php">Tietokanta esimerkki</a></li>
+        </ul>
+    </div>
+    <div id="content">
